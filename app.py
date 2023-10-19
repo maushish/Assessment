d726199696630bfe9428a818bfb315f806b990dd
maushish <chamarnumber102@gmail.com>
2023-10-19 07:17:28 +0530

    Build complete with one error regarding txn

--- app.py
+++ app.py
@@ -1,118 +1,46 @@
 from flask import Flask, render_template, request, redirect
 from web3 import Web3
-from dotenv import load_dotenv
-import os
 import json
 
-load_dotenv()
+abi = json.loads('[{"inputs":[{"internalType":"string","name":"description","type":"string"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"addEntry","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"entries","outputs":[{"internalType":"string","name":"description","type":"string"},{"internalType":"uint256","name":"amount","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getEntries","outputs":[{"components":[{"internalType":"string","name":"description","type":"string"},{"internalType":"uint256","name":"amount","type":"uint256"}],"internalType":"struct Ledger.Entry[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"}]')
 
-infura_api = os.getenv("INFURA_API")
-abi = json.loads('''[
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "description",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "addEntry",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "entries",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "description",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getEntries",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "string",
-						"name": "description",
-						"type": "string"
-					},
-					{
-						"internalType": "uint256",
-						"name": "amount",
-						"type": "uint256"
-					}
-				],
-				"internalType": "struct Ledger.Entry[]",
-				"name": "",
-				"type": "tuple[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-]''')
-
-
-contract_add = os.getenv("ADD")
-key = os.getenv("PVT_KEY")
-
+contract_address = "0x57F6eBc68ED66417f09e832cb98fA50c48bcb432"  # Replace with your contract address
+private_key = "051b7dab0e5d6cc6c0a41fa2daecb99ef16ba0e38901ecaef8bc0720c485aa11"  # Replace with your private key
 
 app = Flask(__name__)
 
+w3 = Web3(Web3.HTTPProvider("https://goerli.infura.io/v3/734ed36cb57348b39159e09139f3fc10"))
+
+contract = w3.eth.contract(address=contract_address, abi=abi)
+
 @app.route("/")
 def index():
-    w3 = Web3(Web3.HTTPProvider(infura_api))
-
-    contract_address = contract_add 
-    contract = w3.eth.contract(address=contract_address, abi=abi)  
-
     entries = contract.functions.getEntries().call()
-
-    return render_template("index.html" , entries=entries)
+    return render_template("index.html", entries=entries)
 
 @app.route("/add", methods=["POST"])
 def add_entry():
     description = request.form["description"]
-    amount = int(request.form["amount"])
+    amount = request.form["amount"]
+    amt=int(float(amount))
 
-    w3 = Web3(Web3.HTTPProvider(infura_api))
+    sender_address = "0xD4cd86fC20602Bb54bb76A6052b8a716B1837e79"  # Replace with the sender's Ethereum address
 
-    contract_address = contract_add  
-    contract = w3.eth.contract(address=contract_address, abi=abi)  
+    nonce = w3.eth.get_transaction_count(sender_address)
 
-    tx = contract.functions.addEntry(description, amount).buildTransaction()
-    signed_tx = w3.eth.account.sign_transaction(tx, private_key=key)
-    w3.eth.send_transaction(signed_tx)
+    transaction = contract.functions.addEntry(description, amt).transact({
+        'chainId': 5,
+        'nonce': nonce,
+        'gas': 2000000,
+        'gasPrice': w3.eth.gas_price,
+        'from': sender_address
+    })
+
+    signed_tx = w3.eth.account.signTransaction(transaction, private_key=private_key)
+
+    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+    w3.eth.waitForTransactionReceipt(tx_hash)
 
     entries = contract.functions.getEntries().call()
 
